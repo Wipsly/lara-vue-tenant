d22c46c6ac29f48db9d52ea1d8fbdbc80e79eeee
Wipsly <wipsly@outlook.de>
2016-12-13 20:11:24 +0100

    First Init

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ user.name }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: ''
+            };
+        },
+        methods: {
+            fetchUser() {
+                this.$http.get('/api/user')
+                        .then(response => {
+                            this.user = response.data;
+                        });
+            }
+        },
+        created() {
+            this.fetchUser();
+        }
+    }
+
+</script>
